9adb2a25548122812bb3ca14641b8a8375d04cd8
wengqizhu <wengqizhuLS@jomoo.cn>
2024-07-19 10:57:28 +0800

    搜索位置设置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,26 +9,6 @@ export default defineConfig({
   themeConfig: {
     outlineTitle: "文章目录",
     outline: [2, 6],
-    // 设置搜索框的样式
-    search: {
-      provider: "local",
-      options: {
-        translations: {
-          button: {
-            buttonText: "搜索文档",
-            buttonAriaLabel: "搜索文档",
-          },
-          modal: {
-            noResultsText: "无法找到相关结果",
-            resetButtonTitle: "清除查询条件",
-            footer: {
-              selectText: "选择",
-              navigateText: "切换",
-            },
-          },
-        },
-      },
-    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: "首页", link: "/" },
@@ -59,6 +39,26 @@ export default defineConfig({
     ],
     footer: {
       copyright: "Copyright@ 2018 Albert weng",
+    },
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
     },
   },
 });
